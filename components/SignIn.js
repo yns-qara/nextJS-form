824117d5829f7060214cd6838c2b0abcc8e363cf
yns-qara <younesqara@gmail.com>
2022-10-30 23:02:10 +0100

    🎵 hovers

--- components/SignIn.js
+++ components/SignIn.js
@@ -9,10 +9,35 @@ const SignIn = () => {
     const [email, setEmail] = useState()
     const [password, setpassword] = useState()
 
-    const handleLogin = async () => {
-        const user = axios.post("http://127.0.0.1:8000/api/signIn", { email, password })
-    }
+    // const handleLogin = async () => {
+    //     const user = axios.post("http://127.0.0.1:8000/api/signIn", { email, password })
+    // }
 
+
+    const handleLogin = async () => {
+
+
+        const response = await fetch('http://127.0.0.1:8000/api/signIn', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'accept': 'application/json'
+            },
+            // credentials: 'same-origin',
+            // credentials: 'include',
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+
+        }
+        )
+        
+
+        // await window.localStorage.setItem('token',response.data);
+        // setLogedIn(true);
+
+    }
 
     return (
         <form className={styles.container}
